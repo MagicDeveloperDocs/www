7986d0ea459fa626f552d0cff975b44f625609a9
root <root@ip-172-31-44-101.us-east-2.compute.internal>
2022-07-09 08:02:26 +0000

    init

---
+++ js/lang.js
@@ -0,0 +1,80 @@
+magic cube
+token system
+mc ecology
+video
+buy mgp
+docs
+manage
+community
+news
+audit
+launch APP
+watch the video
+watch the video
+get magic cube updated
+Be the first to hear when we announce big nft drops, important partnerships, and new products. your email will be kept private.
+token system
+The value of the magic cube ecosystem is carried by a system of tokens that circulate in harmony,complementing each other and jointly promoting the sustainable development of the whole ecosystem.with the dual-blockchai mechanism,the package of ecological tokens run on both binance smart chain and ethereum networks.
+mc ecology
+The magic cube ecosystem runs a series of products based on the protocol, covering the features tokenized assets minting, trading, liquidity providing and staking. each product operates independently in an interconnected system to jointly build a powerful defi ecology for magic cube ecosystem.
+crystal ball
+gringotts bank
+ring swap
+dwarf pool
+box staking
+glen bridge
+crystal ball oracle
+The value of all tokenized assets in Magic Cube ecosystem is determined by the Crystal Ball Oracle that drives the price information on blockchains. The oracle generates the mapped value of each asset by the algorithms with multiple sources.
+gringotts bank
+Gringotts Bank is the workshop to mint the mcAsset of Magic Cube platform. Users can mint new mcAsset tokens against their deposited collateral (USDM) via Magic Cube smart contracts.
+ring swap
+Ring Swap is automated market maker (AMM) protocol for on-chain assets trading by smart contracts. Ring Swap provides automated matching mechanism for the assets pairs of USDM -mcAssets, or USDM-MAGC.
+dwarf pool
+You can provide liquidity for the pairs of mcAsset -USDM or MAGC -USDM at the Dwarf Pool. When your asset pair is deposited to Dwarf Mining pool, you will get the LP token that marks the collateral assets.
+box staking
+Box Staking runs the smart contracts with the logics of token staking and rewards distribution. The rewards for LP stakers come from the newly generated MAGC token at each block. The new MAGC tokens are distributed in proportion to size of staked LP tokens multiplied by the weight of that asset's staking pool.
+glen bridge
+Glen Bridge enables cross-chain transfer of the tokens circulating in Magic Cube ecosystem, including MGP, MAGC, USDM, mcAsset(s) and other potential token types from the ecosystem.
+featured videos
+more video
+community
+This is a super powerful community of MAGIC
+The distance has disappeared, either innovation or death.
+-Thomas Peters
+Innovation is the soul of development, while the community, will be the blood of our movement for financial democracy.
+Magic Cube has a community of members in over 100 countries around the world, and we are growing.
+The dawn is just around the corner, and let’s explore new industry trends and write a new chapter in history with Magic Cube community magic power.
+follow us
+vietnamese community
+chinese community
+indonesian community
+korean community
+malaysian community
+africa english community
+arabic community
+russian community
+spanish community
+france community
+nigeria community
+india community
+croatian community
+bangladesh community
+philippines community
+Germany community
+kurdistan community
+portuguese community
+click to select the language of community you prefer
+get magic cube updated
+Subscribe to stay in touch. your email will be kept private.
+contact us
+You can contact us in different ways.
+magic power global
+magic cube eco global
+
+
+
+
+
+
+
+
